ea6308dcc467e59e0c925de0ee590cbbbc7ddc35
myworldbox <54nicholasleung45@gmail.com>
2022-02-16 15:20:53 +0800

    no message

---
+++ canvas.js
@@ -0,0 +1,110 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+};
+
+const colors = [
+  "#ffffff",
+  "#3454da",
+  "#edd8f7",
+  "#4dd4ff",
+  "#fff49e",
+  "#ff9e43",
+];
+
+let mouseMove = false;
+
+addEventListener("mouseout", () => {
+  mouseMove = true;
+});
+
+addEventListener("mousemove", () => {
+  mouseMove = false;
+});
+
+addEventListener("resize", () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
+  init();
+});
+
+class Particle {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.shadowColor = this.color;
+    c.shadowBlur = 12;
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
+let particles;
+function init() {
+  particles = [];
+
+  for (let i = 0; i < 450; i++) {
+    const canvasWidth = canvas.width + 300;
+    const canvasHeight = canvas.height + 300;
+    const x = Math.random() * canvasWidth - canvasWidth / 2;
+    const y = Math.random() * canvasHeight - canvasHeight / 2;
+    const radius = 2 * Math.random();
+
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    particles.push(new Particle(x, y, radius, color));
+  }
+}
+
+let radians = 0;
+let alpha = 1;
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = `rgb(11, 10, 28, ${alpha})`;
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  //c.textAlign = 'center'; 
+  //c.fillStyle = 'gold';
+  //c.fillText("祝你在亂流下平安", canvas.width / 2, canvas.height / 2);
+
+  c.save();
+  c.translate(canvas.width / 2, canvas.height / 2);
+  c.rotate(radians);
+  particles.forEach((particle) => {
+    particle.update();
+  });
+  c.restore();
+
+  if (!mouseMove) {
+    radians -= 0.0055;
+    if (alpha >= 0.1) {
+      alpha -= 0.01;
+    }
+
+    if (mouseMove && alpha < 1) {
+      alpha += 0.015;
+    }
+  }
+}
+
+init();
+animate();
--- index.js
+++ index.js
@@ -1,33 +1,93 @@
-const userCardTemplate = document.querySelector("[data-user-template]")
-const userCardContainer = document.querySelector("[data-user-cards-container]")
 const searchInput = document.querySelector("[data-search]")
 
 let users = []
+let motto = []
+
+let filePath, template, container, operator
 
 searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase()
   users.forEach(user => {
-    const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
+    const isVisible = (user.content0.toLowerCase().includes(value) || user.content1.toLowerCase().includes(value))
     user.element.classList.toggle("hide", !isVisible)
   })
 })
 
-fetch("index.json")
-  .then(res => res.json())
-  .then(data => {
-    users = data.map(user => {
-      const card = userCardTemplate.content.cloneNode(true).children[0]
-      const header = card.querySelector("[data-header]")
-      const body = card.querySelector("[data-body]")
-      
+function fetchJson(filePath, template, container, operator) {
+  let counter = 0
+  fetch(filePath)
+    .then(res => res.json())
+    .then(data => {
+      users = data.map(user => {
+        let card;
+        if (template != "") {
+          card = document.querySelector(template).content.cloneNode(true).children[0]
+        }
+        switch (operator) {
 
-      let html = "<a href='" + user.link + "'>" + user.name + "</a>"
-      header.innerHTML += html
+          case 0:
+            const header1 = card.querySelector("[data-header-1]")
 
-      body.textContent = user.email
-      userCardContainer.append(card)
-      return { name: user.name, email: user.email, element: card }
+            const body1 = card.querySelector("[data-body-1]")
+
+            const body2 = card.querySelector("[data-body-2]")
+
+            header1.textContent = user.content0
+
+            let html = "<a class='border-1 padding-1 p3-1 bg-color-6' href='" + user.content1 + "'>" + counter + "</a>"
+            body1.innerHTML += html
+            
+            body2.textContent = user.content2
+
+            break;
+
+          case 1:
+
+            motto[counter] = (user.content0)
+            break;
+
+          case 2:
+
+            const header = card.querySelector("[data-header]")
+            const body = card.querySelector("[data-body]")
+            
+            header.innerHTML += "<div class='div-container'><div class='padding-1 border-4' style='background:white;'><div class='link' style='background: url(" + user.content1 + ");'><a href='" + user.content2 + "'></a></div></div></div>"
+
+            body.textContent = user.content0
+
+            break;
+        }
+        
+        counter = counter + 1
+
+        if (container != "") {
+          document.querySelector(container).append(card)
+          return { content0: user.content0, content1: user.content1, content2: user.content2, element: card }
+        }
+      })
     })
-  })
+}
+
+fetchJson("./json/VL-project.json", "[data-user-template-1]", "[data-user-cards-container-1]", 0)
+fetchJson("./json/VL-motto.json", "", "", 1)
+fetchJson("./json/VL-link.json", "[data-user-template]", "[data-user-cards-container]", 2)
+
+async function flash() {
+  for (var i = 0; i < 1000;) {
+
+    console.log(motto.length)
+    document.getElementsByClassName("absolute-style")[0].innerHTML = motto[Math.floor(Math.random() * motto.length) + 0];
+    await new Promise((r) => setTimeout(r, Math.floor(Math.random() * 50) + 10));
+    i++;
+    if (i % 10 == 0) {
+      document.getElementsByClassName("absolute-style")[0].innerHTML = motto[Math.floor(Math.random() * motto.length) + 0];
+      await new Promise((r) => setTimeout(r, 5000));
+    }
+  }
+}
+
+window.onload = function () {
+  setTimeout(() => {
+    flash();
+  }, 5000);
+}
