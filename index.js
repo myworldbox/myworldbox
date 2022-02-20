86340b62cb2b5f47c477c90e09fcc1b576a337e6
myworldbox <54nicholasleung45@gmail.com>
2022-02-21 00:03:43 +0800

    no message

--- index.js
+++ index.js
@@ -1,76 +1,72 @@
-const searchInput = document.querySelector("[data-search]")
-
 let users = []
 let motto = []
+var contentArr = [[], [], []]
 
 let filePath, template, container, operator
 
-searchInput.addEventListener("input", e => {
-  const value = e.target.value.toLowerCase()
-  users.forEach(user => {
-    const isVisible = (user.content0.toLowerCase().includes(value) || user.content2.toLowerCase().includes(value))
-    user.element.classList.toggle("hide", !isVisible)
-  })
-})
+let card
+let html
+
+let jsonFile = 3, jsonContent = 3;
+
+const searchInput = document.querySelector("[search-1]")
 
 function fetchJson(filePath, template, container, operator) {
+
   let counter = 0
   fetch(filePath)
     .then(res => res.json())
     .then(data => {
-      users = data.map(user => {
-        let card;
-        if (template != "") {
+      let temp = data.map(user => {
+        if (document.querySelector(template)) {
           card = document.querySelector(template).content.cloneNode(true).children[0]
+          for (var j = 0; j < jsonContent; j++) {
+            if (card.querySelector("[content" + j + "-" + operator + "]")) {
+              contentArr[j][operator] = card.querySelector("[content" + j + "-" + operator + "]")
+            }
+          }
         }
+
         switch (operator) {
 
           case 0:
-            const header1 = card.querySelector("[data-header-1]")
 
-            const body1 = card.querySelector("[data-body-1]")
+            html = "<a class='border-1 padding-1 p3-1 bg-color-6' href='" + user.content1 + "'>" + counter + "</a>"
 
-            const body2 = card.querySelector("[data-body-2]")
-
-            header1.textContent = user.content0
-
-            let html = "<a class='border-1 padding-1 p3-1 bg-color-6' href='" + user.content1 + "'>" + counter + "</a>"
-            body1.innerHTML += html
-
-            body2.textContent = user.content2
+            contentArr[0][operator].textContent = user.content0
+            contentArr[1][operator].innerHTML += html
+            contentArr[2][operator].textContent = user.content2
 
             break;
 
           case 1:
 
-            motto[counter] = (user.content0)
+            contentArr[0][operator].innerHTML += "<div class='div-container'><div class='padding-1 border-4' style='background:white;'><div class='link' style='background: url(" + user.content1 + ");'><a href='" + user.content2 + "'></a></div></div></div>"
+            contentArr[1][operator].textContent = user.content0
+
             break;
 
           case 2:
 
-            const header = card.querySelector("[data-header]")
-            const body = card.querySelector("[data-body]")
-
-            header.innerHTML += "<div class='div-container'><div class='padding-1 border-4' style='background:white;'><div class='link' style='background: url(" + user.content1 + ");'><a href='" + user.content2 + "'></a></div></div></div>"
-
-            body.textContent = user.content0
+            motto[counter] = (user.content0)
 
             break;
         }
 
         counter = counter + 1
 
-        if (container != "") {
+        if (document.querySelector(container)) {
           document.querySelector(container).append(card)
           return { content0: user.content0, content1: user.content1, content2: user.content2, element: card }
         }
       })
+      if(operator == 1) users = temp
     })
 }
 
-fetchJson("./json/VL-project.json", "[data-user-template-1]", "[data-user-cards-container-1]", 0)
-fetchJson("./json/VL-motto.json", "", "", 1)
-fetchJson("./json/VL-link.json", "[data-user-template]", "[data-user-cards-container]", 2)
+for (var i = 0; i < jsonFile; i++) {
+  fetchJson("./json/VL-" + i + ".json", "[template-" + i + "]", "[container-" + i + "]", i)
+}
 
 async function flash() {
   for (var i = 1; i >= 1; i++) {
@@ -83,6 +79,14 @@ async function flash() {
     }
   }
 }
+
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase()
+  users.forEach(user => {
+    const isVisible = (user.content0.toLowerCase().includes(value) || user.content2.toLowerCase().includes(value))
+    user.element.classList.toggle("hide", !isVisible)
+  })
+})
 
 window.onload = () => {
   flash();
