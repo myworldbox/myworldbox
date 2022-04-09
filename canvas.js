0bbd4be33971d6169d6c30f8b1cf5cd2c576f128
myworldbox <forkterminal@gmail.com>
2022-04-10 01:09:24 +0800

    no message

--- canvas.js
+++ canvas.js
@@ -128,5 +128,5 @@ async function fetch() {
 
 function accessCheck() {
     console.log(window.top.location.href);
-    (window.top.location.href != "https://myworldbox.vercel.app") ? fetch() : (document.body.innerHTML = "Access Denied<br/><br/>Go to<br/><a href='https://myworldbox.github.io' class='color-2'>myworldbox</a><br/>for more information");
+    (window.top.location.href != "https://myworldbox.vercel.app/") ? fetch() : (document.body.innerHTML = "Access Denied<br/><br/>Go to<br/><a href='https://myworldbox.github.io' class='color-2'>myworldbox</a><br/>for more information");
 }
