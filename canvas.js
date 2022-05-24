35150a2045d20ea04bc795b4b9314ae039c8d215
myworldbox <54nicholasleung45@gmail.com>
2022-05-25 02:14:12 +0800

    no message

--- canvas.js
+++ canvas.js
@@ -1,13 +1,12 @@
-const canvas = document.querySelector("canvas");
-const c = canvas.getContext("2d");
-
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+let mouseMove = false
+let radians = 0
+let particles
+let alpha = 1
 
 const mouse = {
     x: innerWidth / 2,
     y: innerHeight / 2,
-};
+}
 
 const colors = [
     "#ffffff",
@@ -16,94 +15,91 @@ const colors = [
     "#4dd4ff",
     "#fff49e",
     "#ff9e43",
-];
+]
 
-let mouseMove = false;
+const canvas = document.querySelector("canvas")
+const c = canvas.getContext("2d")
 
 addEventListener("mouseout", () => {
-    mouseMove = true;
-});
+    mouseMove = true
+})
 
 addEventListener("mousemove", () => {
-    mouseMove = false;
-});
+    mouseMove = false
+})
 
 addEventListener("resize", () => {
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
-
-    init();
-});
+    init()
+})
 
 class Particle {
     constructor(x, y, radius, color) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.color = color;
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
     }
-
     draw() {
-        c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.shadowColor = this.color;
-        c.shadowBlur = 12;
-        c.fillStyle = this.color;
-        c.fill();
-        c.closePath();
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.shadowColor = this.color
+        c.shadowBlur = 12
+        c.fillStyle = this.color
+        c.fill()
+        c.closePath()
     }
-
     update() {
-        this.draw();
+        this.draw()
     }
 }
 
-let particles;
+var init = () => {
 
-function init() {
-    particles = [];
+    canvas.width = innerWidth
+    canvas.height = innerHeight
+
+    particles = []
 
     for (let i = 0; i < 450; i++) {
-        const canvasWidth = canvas.width + 300;
-        const canvasHeight = canvas.height + 300;
-        const x = Math.random() * canvasWidth - canvasWidth / 2;
-        const y = Math.random() * canvasHeight - canvasHeight / 2;
-        const radius = 2 * Math.random();
+        const canvasWidth = canvas.width + 300
+        const canvasHeight = canvas.height + 300
+        const x = Math.random() * canvasWidth - canvasWidth / 2
+        const y = Math.random() * canvasHeight - canvasHeight / 2
+        const radius = 2 * Math.random()
 
-        const color = colors[Math.floor(Math.random() * colors.length)];
-        particles.push(new Particle(x, y, radius, color));
+        const color = colors[Math.floor(Math.random() * colors.length)]
+        particles.push(new Particle(x, y, radius, color))
     }
 }
 
-let radians = 0;
-let alpha = 1;
+var animate = () => {
+    requestAnimationFrame(animate)
+    c.fillStyle = `rgb(11, 10, 28, ${alpha})`
+    c.fillRect(0, 0, canvas.width, canvas.height)
 
-function animate() {
-    requestAnimationFrame(animate);
-    c.fillStyle = `rgb(11, 10, 28, ${alpha})`;
-    c.fillRect(0, 0, canvas.width, canvas.height);
-
-    c.save();
-    c.translate(canvas.width / 2, canvas.height / 2);
-    c.rotate(radians);
+    c.save()
+    c.translate(canvas.width / 2, canvas.height / 2)
+    c.rotate(radians)
     particles.forEach((particle) => {
-        particle.update();
-    });
-    c.restore();
+        particle.update()
+    })
+    c.restore()
 
     if (!mouseMove) {
-        radians -= 0.0055;
+        radians -= 0.0055
         if (alpha >= 0.1) {
-            alpha -= 0.01;
+            alpha -= 0.01
         }
 
         if (mouseMove && alpha < 1) {
-            alpha += 0.015;
+            alpha += 0.015
         }
     }
 }
 
-window.onload = () => (window.top.location.href != "https://myworldbox.vercel.app/") ? 1 : (document.body.innerHTML = "Access Denied<br/><br/>Go to<br/><a href='https://myworldbox.github.io' class='color-2'>myworldbox</a><br/>for more information");
+(window.top.location.href != "https://myworldbox.vercel.app/") ? 1 : (document.body.innerHTML = "Access Denied<br/><br/>Go to<br/><a href='https://myworldbox.github.io' class='color-2'>myworldbox</a><br/>for more information")
 
-init();
-animate();
+window.onload = () => {
+    init()
+    animate()
+}
