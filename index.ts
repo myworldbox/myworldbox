d9a623592e4d042f7391c2216363423ec878d570
myworldbox <54nicholasleung45@gmail.com>
2022-12-15 02:50:16 +0800

    no message

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ var state = true
 var searchInput = document.querySelector("[search-1]")
 
 var inject = (url: any) => {
+    
     if (document.getElementById("code")) {
         var qrcodes = new QRCode({
             msg: url, dim: 256, pad: 4, mtx: -1, ecl: "H", ecb: 1, pal: ["#037", "#f2f4f8"], vrb: 0
