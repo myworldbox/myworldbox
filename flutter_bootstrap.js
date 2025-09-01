bbaa9403f680d4497306744d93d76308d41f42cc
myworldbox <54nicholasleung45@gmail.com>
2025-09-02 02:23:41 +0800

    fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"3340caed81328f111a659fc66632e1bebaf865
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "881976404"
+    serviceWorkerVersion: "3303684900"
   }
 });
