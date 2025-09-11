696eae43d23d58a577fd201a868414bd149ed396
myworldbox <54nicholasleung45@gmail.com>
2025-09-11 20:58:47 +0800

    fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -128,14 +128,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "57430d4fb866894e7363203cd567571d
 "deploy.html": "a5cf6500e2c3857b3fb785e06e770d1b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "35408b1be0f5de68cc5d1c5175d717ed",
-"flutter_bootstrap.js": "cd7a97808d742183a33cd164d2ddf8d2",
+"flutter_bootstrap.js": "3727137c4d6a812d0ffb7585a70ffb54",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "328b6766b986cde6e21cd4efbb0277f0",
 "/": "328b6766b986cde6e21cd4efbb0277f0",
-"main.dart.js": "3ee16290d291710f36946f55fa46908d",
+"main.dart.js": "f759537194e7fc1a1f0fe7e57032d419",
 "manifest.json": "ba5c7f54abd807b20aa52b239bb3adb5",
 "version.json": "9692e6a2dac4ad01af0fd4a3db0756bd"};
 // The application shell files that are downloaded before a service worker can
